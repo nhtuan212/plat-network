99b05fc63beac7a3989782ae87fdb18b40173cc5
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2024-12-21 08:40:52 +0700

    Update signMessage

--- src/components/layouts/HomePage.tsx
+++ src/components/layouts/HomePage.tsx
@@ -1,38 +1,34 @@
-// "use client";
+"use client";
 
 import React from "react";
 import Button from "@/components/Button";
 
-export default async function HomePage() {
-    //** Functions */
-    const getProvider = () => {
-        if (typeof window !== "undefined" && "phantom" in window) {
-            const anyWindow: any = window;
-            const provider = anyWindow.phantom?.ethereum;
-
-            if (provider) {
-                return provider;
-            }
-
-            window.open("https://phantom.app/", "_blank");
-        }
-    };
-
-    const provider = getProvider();
-    let signedMessage;
-
-    if (provider) {
-        const message = `To avoid digital dognappers, sign below to authenticate with CryptoCorgis`;
-        const encodedMessage = new TextEncoder().encode(message);
-        signedMessage = await provider.signMessage(encodedMessage, "utf8");
-    } else {
-        console.error("Provider not found");
-    }
-
+export default function HomePage() {
     //** Render */
     return (
         <div className="h-screen flex justify-center items-center">
-            <Button size="lg">Click me</Button>
+            <Button
+                size="lg"
+                onPress={async () => {
+                    const encodedMessage = new TextEncoder().encode(
+                        "Message to sign"
+                    );
+
+                    await window.solana
+                        .request({
+                            method: "signMessage",
+                            params: {
+                                message: encodedMessage,
+                                display: "utf8", //hex,utf8
+                            },
+                        })
+                        .then((response) => {
+                            console.log(response);
+                        });
+                }}
+            >
+                Click me
+            </Button>
         </div>
     );
 }
